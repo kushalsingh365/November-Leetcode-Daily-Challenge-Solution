2171021a6e43783e03e83f1a1930f99b3f41516d
Kushal Singh <81277166+kushalsingh365@users.noreply.github.com>
2022-11-11 21:49:27 +0530

    Remove Duplicates from Sorted Array

---
+++ 11_Nov_22.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        
+        int i=0, j=i+1;
+        while(j<nums.size()){
+            if(nums[i] != nums[j]){
+                swap(nums[i+1], nums[j]);
+                i++;
+            }
+            j++;
+        }
+        
+        return i+1;
+    }
+};
